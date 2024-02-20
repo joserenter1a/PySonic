7aa64edf069f8b8b06d5d3b2947f7fa1fa1b265f
carson-hellman <carsonie24@gmail.com>
2024-02-20 13:51:55 -0800

    attempted to factor out grid system. Not getting gid in correct format for react element

--- pysonic/src/components/Grid.jsx
+++ pysonic/src/components/Grid.jsx
@@ -1,25 +1,34 @@
 import * as React from 'react';
 import { useState } from 'react';
+
+const GenText = () => {
+  return (<input type="text" id="name" name="name" required minLength="4" maxLength="8" size="10"/>);
+}
+
+const MakeGrid = ({height, width}) => {
+  const row = [];
+    for (let i = 0; i < width; i++) {
+      row.push(<GenText key={i}/>);
+    }
+  return (<row/>);
+}
+
 export const Grid = () => {
     // javascript variables that can be used in your components i.e. count
     const [count, setCount] = useState(0);
     const [text, secText] = useState("");
-    let col = [];
-    for (let i = 0; i < 100; i++){
-      let row = [];
-      for (let j = 0; j < 8; j++){
-        row.push([]);
-      }
-      col.push(row);
-    }
-    console.log(col);
+    
+    const CompletedGrid = MakeGrid(5, 5);
+    console.log(CompletedGrid);
     // the return statement only returns html/xml components
     return (   
       <div>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <div><input type="text" id="name" name="name" required minLength="4" maxLength="8" size="10"/>
+
+        <div>
+        </div>
+
+        <div>
+        <input type="text" id="name" name="name" required minLength="4" maxLength="8" size="10"/>
         <input type="text" id="name" name="name" required minLength="4" maxLength="8" size="10"/>
         <input type="text" id="name" name="name" required minLength="4" maxLength="8" size="10"/>
         <input type="text" id="name" name="name" required minLength="4" maxLength="8" size="10"/></div>
