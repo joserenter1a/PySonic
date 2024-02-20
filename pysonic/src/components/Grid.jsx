5e47e723ca1d8b8df702839c6772d4dc6740e42b
Joseph Macalinao <jhmac121101@gmail.com>
2024-02-20 12:44:45 -0800

    added grid

--- pysonic/src/components/Grid.jsx
+++ pysonic/src/components/Grid.jsx
@@ -1,15 +1,42 @@
 import * as React from 'react';
-import { useState } from 'react'
+import { useState } from 'react';
 export const Grid = () => {
     // javascript variables that can be used in your components i.e. count
     const [count, setCount] = useState(0);
-
-    
+    const [text, secText] = useState("");
+    let col = [];
+    for (let i = 0; i < 100; i++){
+      let row = [];
+      for (let j = 0; j < 8; j++){
+        row.push([]);
+      }
+      col.push(row);
+    }
+    console.log(col);
     // the return statement only returns html/xml components
     return (   
+      <div>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>    
+        </button>
+        <div><input type="text" id="name" name="name" required minLength="4" maxLength="8" size="10"/>
+        <input type="text" id="name" name="name" required minLength="4" maxLength="8" size="10"/>
+        <input type="text" id="name" name="name" required minLength="4" maxLength="8" size="10"/>
+        <input type="text" id="name" name="name" required minLength="4" maxLength="8" size="10"/></div>
+        <div><input type="text" id="name" name="name" required minLength="4" maxLength="8" size="10"/>
+        <input type="text" id="name" name="name" required minLength="4" maxLength="8" size="10"/>
+        <input type="text" id="name" name="name" required minLength="4" maxLength="8" size="10"/>
+        <input type="text" id="name" name="name" required minLength="4" maxLength="8" size="10"/></div>
+        <div><input type="text" id="name" name="name" required minLength="4" maxLength="8" size="10"/>
+        <input type="text" id="name" name="name" required minLength="4" maxLength="8" size="10"/>
+        <input type="text" id="name" name="name" required minLength="4" maxLength="8" size="10"/>
+        <input type="text" id="name" name="name" required minLength="4" maxLength="8" size="10"/></div>
+        <div><input type="text" id="name" name="name" required minLength="4" maxLength="8" size="10"/>
+        <input type="text" id="name" name="name" required minLength="4" maxLength="8" size="10"/>
+        <input type="text" id="name" name="name" required minLength="4" maxLength="8" size="10"/>
+        <input type="text" id="name" name="name" required minLength="4" maxLength="8" size="10"/></div>
+
+      </div>
     );
 
 };
