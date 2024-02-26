8cdb34e3c769731b80606db0a5e5493cac1d4d99
joserenter1a <josevt209@gmail.com>
2024-02-26 14:02:54 -0800

    Implemented out file functionality

--- pysonic/src/App.jsx
+++ pysonic/src/App.jsx
@@ -1,28 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css'
 // import custom component to embed in our application container
-import { Grid } from './components/Grid';
-import Compiler from './compiler/Compiler';
+import GridComponent  from './components/Grid';
 
 function App() {
 
   return (
     <>
       <img src="/PySonicLogo.png" alt="Description of the image" width="193" height="130"></img>
-      <h1>Home Page for da Code Editor</h1>
-      <div className="card">
-          <div>
-            <Grid/>
-          </div>
-        <div>
-          <Compiler/>
+      <div >
+        
+          <div
+            style={gridStyle}
+          >
+            <GridComponent
 
-        </div>
+            />
+          </div>
       </div>
 
 
     </>
   )
 }
+const gridStyle ={
+  overflowY: 'auto',
+ backgroundColor:'black', 
+ padding: '15px', 
+ height: '500px',
+ width: '1000px'
+}
+export default App;
 
-export default App
+
--- pysonic/src/components/Grid.jsx
+++ pysonic/src/components/Grid.jsx
@@ -1,8 +1,15 @@
 import * as React from 'react';
 import { useState } from 'react';
+import Editor from 'react-simple-code-editor';
+import { highlight, languages } from 'prismjs/components/prism-core';
+import 'prismjs/components/prism-clike';
+import 'prismjs/components/prism-javascript';
+import 'prismjs/components/prism-python';
+import axios from 'axios';
 
-const setHeight = 10;
-const setWidth = 5;
+import 'prismjs/themes/prism.css';
+const setHeight = 25;
+const setWidth = 4;
 
 const listener = (e, row, col) =>{
   const textInput = document.getElementById(`${row}-${col}`);
@@ -16,27 +23,84 @@ const listener = (e, row, col) =>{
       else {
         // move focus to next line
         const rowDownText = document.getElementById(`${row+1}-${0}`);
+        //textInput.append('\n');
+        console.log(textInput);
         rowDownText.focus();
       }
     }
-    else if (event.key === 'Tab'){
-      if (col == setWidth-1){
-        // console.log("At last col, don't tab to next row");
-        event.preventDefault();
-      }
+    else if (event.key === 'F1' )
+    {
+      const rowDownText = document.getElementById(`${0}-${0}`);
+      rowDownText.focus();
     }
   });
 }
 
 
-const GridComponent = ({height, width}) => {
+export const GridComponent = () => {
+  const [code, setCode] = useState('');
 
+  const [output, setOutput] = useState('');
+  const [isLoadingCompile, setIsLoadingCompile] = useState(false);
+  const [isLoadingSubmit, setIsLoadingSubmit] = useState(false);
+  const [selectedLanguage, setSelectedLanguage] = useState('5'); // Default to Python (value 5)
+  const [error, setError] = useState('');
+  
+  const handleRun = async () => {
+    setIsLoadingCompile(true);
+    setError(''); // Clear any previous errors
+
+    const encodedParams = new URLSearchParams();
+    encodedParams.set('LanguageChoice', selectedLanguage);
+    encodedParams.set('Program', code);
+
+    const options = {
+      method: 'POST',
+      url: 'https://code-compiler.p.rapidapi.com/v2',
+      headers: {
+        'content-type': 'application/x-www-form-urlencoded',
+        'X-RapidAPI-Key': 'c40e63ca05msh21cc53be5c61ed5p1be771jsnda85c9acad28',
+        'X-RapidAPI-Host': 'code-compiler.p.rapidapi.com'
+      },
+      data: encodedParams,
+    };
+
+    try {
+      const response = await axios.request(options);
+      const result = response.data.Result;
+      const errorOutput = response.data.Errors;
+
+      if (errorOutput) {
+        setOutput(errorOutput + '\n' + result);
+        setError(errorOutput);
+      } else {
+        setOutput(result);
+        setError('');
+      }
+    } catch (error) {
+      console.error(error);
+      setError('Error compiling code. Please check your code and try again.');
+    }
+
+    setIsLoadingCompile(false);
+  };
+
+  const handleSubmit = async () => {
+    setIsLoadingSubmit(true);
+    const userCode = code;
+    const finalOutput = `There are errors on lines....${userCode}`
+
+    // want to move user focus here to output so screen reader can announce errors
+
+    setOutput(finalOutput)
+    setIsLoadingSubmit(false)
+  };
   // useState hook to push into an empty array
   const [array, setArray] = useState(() => {
       const emptyArray = [];
-      for(let i = 0; i < height; i ++)
+      for(let i = 0; i < setHeight; i ++)
       {
-        emptyArray.push(Array(width).fill(''));
+        emptyArray.push(Array(setWidth).fill(''));
       }
       return emptyArray;
     }
@@ -48,46 +112,92 @@ const GridComponent = ({height, width}) => {
     i === rowIndex ? row.map((cell, j) => (j === colIndex ? e.target.value : cell)) : row
     );
     setArray(newArray);
+    setCode(newArray.map(row => row.join(' ')).join('\n'));
+
   }
   // returns the array maps to a div with a text input for each cell, with the key
   // being the difference of indices
   return (
+    <div style={{display:'flex'}}>
+
     <div>
+
     {array.map((row, rowIndex) => (
       <div key={rowIndex}>
+          <div style = {{float:'left'}}>{rowIndex + 1}</div>
+
         {row.map((cell, colIndex) => (
           <input
             key={`${rowIndex}-${colIndex}`}
             id= {`${rowIndex}-${colIndex}`}
             type="text"
             value={cell}
+            size = {10}
             onChange={(e) => handleChange(e, rowIndex, colIndex)}
             onFocus={(e) => listener(e, rowIndex, colIndex)}
           />
         ))}
       </div>
     ))}
+
   </div>
-);
-};
-
-export const Grid = () => {
-    // javascript variables that can be used in your components i.e. count
-    const [count, setCount] = useState(0);
-    const [text, secText] = useState("");
-    const [focused, setFocus] = useState(false);
-        // the return statement only returns html/xml components
-    return (   
-      <div>
-
-        <div>
-          <GridComponent
-            height = {setHeight}
-            width = {setWidth}
-          />
+    <div
+      style={{float:'right', paddingLeft: '10px'}}
+      >        
+        <Editor 
+          value={code}
+          onValueChange={code => setCode(code)}
+          highlight={code => highlight(code, languages.python)}
+          padding={10}
+          readOnly
         
-        </div>
+        />
+    </div>
+    
+    <div
+          className='output'
+          style={{ display: 'flex', gap: '10px', width: '150px', height: '300px' }}
+        >
+        {/* Text area for code output */}
+        <textarea
+          className="output"
+          id="output"
+          name="output"
+          readOnly
+          value={output}
+          style={{              
+            fontFamily: '"Fira code", "Fira Mono", monospace',
+            fontSize: 14,
+            color: error ? 'red' : 'black',
+            backgroundColor: error ? '#ffebeb' : 'white',
+          }}
+          />
+      
       </div>
-    );
+    <div className="language-buttons-container">
+        <center>
+          {/* Compilation Button */}
+          <button 
+            type="button" 
+            className="CompileCheckButtons" 
+            onClick={handleRun} 
+            disabled={isLoadingCompile}>
+            {isLoadingCompile ? 'Compiling...' : 'Compile'}
+          </button>
 
-};
+          {/* Check Error Button */}
+          <button 
+            type="button" 
+            className="CompileCheckButtons" 
+            onClick={handleSubmit} 
+            disabled={isLoadingSubmit}>
+            {isLoadingSubmit ? 'Checking for errors...' : 'Check for errors'}
+          </button>
+        </center>
+      </div>
+      
+    </div>
+);
+}
+
+export default GridComponent;
